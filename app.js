dcc4cb4e1b3360f50cc76bccc76e089a66f10fdb
Sakhavat Fleydanov <sexavetfv@gmail.com>
2026-01-31 14:24:23 +0400

    complated with loading effect

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+let i = 0;
+const loading = document.querySelector("#loading");
 const fileInput = document.querySelector("#file__input");
 const preview = document.querySelector(".preview");
+const reader = new FileReader();
+reader.addEventListener("load", () => {
+  const prevImg = document.createElement("img");
+  prevImg.classList.add("prevImg");
+  prevImg.setAttribute("src", reader.result);
+  preview.append(prevImg);
+  i++;
+  readFile();
+});
+
+const readFile = () => {
+  const file = fileInput.files[i];
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    loading.classList.remove("active");
+  }
+};
 fileInput.addEventListener("change", (e) => {
-  [...e.target.files].forEach((file)=>{
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-      const prevImg = document.createElement("img");
-      prevImg.classList.add("prevImg");
-      prevImg.setAttribute("src", reader.result);
-      preview.append(prevImg);
-    });
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  })
+  loading.classList.add("active");
+  preview.textContent = "";
+  readFile();
 });
